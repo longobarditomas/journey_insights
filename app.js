2d6400d2723d2e374a52d7c08a1cff4eed372c49
longobarditomas <longobarditomas@gmail.com>
2024-05-22 01:06:21 -0300

    NewsAPI

--- app.js
+++ app.js
@@ -17,18 +17,41 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-/* OPEN WEATHER */
+const getWeatherData = async (city, country) => {
+  const token = process.env.OPEN_WEATHER_API_KEY;
+  const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${token}`, { timeout: 10000 });
+  //const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${token}`, {timeout : 10000 });
+  return response.data;
+};
 
-app.get("/openweather", async (req, res, next) => {
+const getNewsData = async (country) => {
+  const token = process.env.NEWS_API_KEY;
+  const config = {
+    headers : {
+        'Authorization' : token,
+        'Content-type' : "application/json"
+    }
+  }
+  const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&category=entertainment`, config, { timeout: 10000 });
+  return response.data;
+};
+
+app.get("/data", async (req, res, next) => {
   try{
-    const token = process.env.OPEN_WEATHER_API_KEY;
     const lat = req.query.lat || "-34.607741";
     const lon = req.query.lon || "-58.438561";
-    const city = req.query.lon || "london";
-    const country = req.query.lon || "UK";
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}k&appid=${token}`, {timeout : 10000 });
-    //const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${token}`, {timeout : 10000 });
-    res.status(200).send(response.data);
+    const city = req.query.city || "london";
+    const country = req.query.country || "UK";
+    
+    const weatherData = await getWeatherData(city, country);
+    const newsData    = await getNewsData("gb");
+
+    const data = {
+      weather: weatherData,
+      news: newsData,
+    };
+    res.status(200).send(data);
+    
   } catch (error) {
     next(error);
   }

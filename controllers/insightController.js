37b6e7d357e7dc5e34189986d3ee5def6048e778
longobarditomas <longobarditomas@gmail.com>
2024-06-30 23:45:49 -0300

    CountryInsights Relevant Data

--- controllers/insightController.js
+++ controllers/insightController.js
@@ -15,12 +15,41 @@ const getCountryInsights = async (req, res, next) => {
 
     const openCageData = await getOpenCageData(city, country);
 
-    const data = {
+    const currency = openCageData.results[0].annotations.currency; 
+
+    const csountryInsights = {
+      city : city,
+      country : country,
+      flag : openCageData.results[0].annotations.flag,
+      coordinates : weatherData.coord,
+      weather : {
+        main : weatherData.weather[0].main,
+        description : weatherData.weather[0].description,
+        icon : weatherData.weather[0].icons,
+        temp : {
+          temp : weatherData.main.temp,
+          feels_like : weatherData.main.feels_like,
+          temp_min : weatherData.main.temp_min,
+          temp_max : weatherData.main.temp_max,
+        },
+        wind : weatherData.wind,
+      },
+      currency : {
+        name : currency.name,
+        iso_code : currency.iso_code,
+        iso_numeric : currency.iso_numeric,
+        symbol : currency.symbol,
+      },
+      news : newsData.articles,
+    }
+
+    /* const data = {
+      data: csountryInsights,
       weather: weatherData,
       news: newsData,
       openCage: openCageData,
-    };
-    res.status(200).send(data);
+    }; */
+    res.status(200).send(csountryInsights);
 
   } catch (error) {
     next(error);

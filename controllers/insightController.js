2e6d0b32b7a67ab3287026430379f1c51049b674
longobarditomas <longobarditomas@gmail.com>
2024-07-01 00:00:59 -0300

    Hotfix

--- controllers/insightController.js
+++ controllers/insightController.js
@@ -6,54 +6,60 @@ const getCountryInsights = async (req, res, next) => {
   try {
     const city = req.query.city || "london";
     const country = req.query.country || "UK";
-
-    const weatherData = await getWeatherData(city, country);
-    
-    let newsData = {};
-    if (weatherData.sys.country)
-      newsData = await getNewsData(weatherData.sys.country);
-
-    const openCageData = await getOpenCageData(city, country);
-
-    const currency = openCageData.results[0].annotations.currency; 
-
-    const csountryInsights = {
-      city : city,
-      country : country,
-      flag : openCageData.results[0].annotations.flag,
-      coordinates : weatherData.coord,
-      weather : {
-        main : weatherData.weather[0].main,
-        description : weatherData.weather[0].description,
-        icon : weatherData.weather[0].icons,
-        temp : {
-          temp : weatherData.main.temp,
-          feels_like : weatherData.main.feels_like,
-          temp_min : weatherData.main.temp_min,
-          temp_max : weatherData.main.temp_max,
-        },
-        wind : weatherData.wind,
-      },
-      currency : {
-        name : currency.name,
-        iso_code : currency.iso_code,
-        iso_numeric : currency.iso_numeric,
-        symbol : currency.symbol,
-      },
-      news : newsData.articles,
-    }
-
-    /* const data = {
-      data: csountryInsights,
-      weather: weatherData,
-      news: newsData,
-      openCage: openCageData,
-    }; */
-    res.status(200).send(csountryInsights);
-
+  
+    const countryInsights = await getInsights(city, country);
+    res.status(200).send(countryInsights);
   } catch (error) {
     next(error);
   }
 };
+
+async function getInsights(city, country) {
+  const weatherData = await getWeatherData(city, country);
+    
+  let newsData = {};
+  if (weatherData.sys.country)
+    newsData = await getNewsData(weatherData.sys.country);
+
+  const openCageData = await getOpenCageData(city, country);
+
+  const weather  = parseWeatherData(weatherData);   
+  const currency = parseCurrencyData(openCageData.results[0].annotations.currency); 
+
+  const countryInsights = {
+    city : city,
+    country : country,
+    flag : openCageData.results[0].annotations.flag,
+    coordinates : weatherData.coord,
+    weather : weather,
+    currency : currency,
+    news : newsData.articles,
+  }
+  return countryInsights;
+}
+
+function parseWeatherData(weatherData) {
+  return {
+    main : weatherData.weather[0].main,
+    description : weatherData.weather[0].description,
+    icon : weatherData.weather[0].icons,
+    temp : {
+      temp : weatherData.main.temp,
+      feels_like : weatherData.main.feels_like,
+      temp_min : weatherData.main.temp_min,
+      temp_max : weatherData.main.temp_max,
+    },
+    wind : weatherData.wind,
+  }
+}
+
+function parseCurrencyData(currencyData) {
+  return {
+    name : currencyData.name,
+    iso_code : currencyData.iso_code,
+    iso_numeric : currencyData.iso_numeric,
+    symbol : currencyData.symbol,
+  }
+}
 
 module.exports = { getCountryInsights };
